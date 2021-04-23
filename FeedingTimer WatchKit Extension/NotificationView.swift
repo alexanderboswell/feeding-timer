0fe6e7ed0faec633926c9ef68c11fb18170cf363
Alex Boswell <alexanderboswell@gmail.com>
2021-04-23 06:38:34 -0700

    Initial Commit

---
+++ FeedingTimer WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  FeedingTimer WatchKit Extension
+//
+//  Created by alexander boswell on 4/23/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
